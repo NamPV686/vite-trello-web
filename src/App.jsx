370c9b01fd3821406d28e054ed9ddcd4a25a6db4
NamPV686 <nampv686[gmail.com>
2023-11-17 10:13:27 +0700

    #6 config_mui_theme

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Button from '@mui/material/Button'
 import HomeIcon from '@mui/icons-material/home'
 import { pink } from '@mui/material/colors/'
 import AccountBalance from '@mui/icons-material/accountbalance'
+import Typography from '@mui/material/Typography'
 
 function App() {
 
@@ -9,6 +10,10 @@ function App() {
     <>
       <Button variant="contained">Hello world</Button>
       <p>Hello</p>
+      <Typography variant="body2" color="text.red">
+        h1. Heading
+      </Typography>
+
       <HomeIcon />
       <HomeIcon color="primary" />
       <HomeIcon color="secondary" />
