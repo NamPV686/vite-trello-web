a677933d8d9830c2086d2324c8595e261eda4b77
NamPV686 <nampv686[gmail.com>
2023-11-17 23:32:13 +0700

    #7 Dark-light 1

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,27 @@ import HomeIcon from '@mui/icons-material/home'
 import { pink } from '@mui/material/colors/'
 import AccountBalance from '@mui/icons-material/accountbalance'
 import Typography from '@mui/material/Typography'
+import { useColorScheme } from '@mui/material/styles'
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
 
 function App() {
 
   return (
     <>
+      <ModeToggle />
+      <hr />
       <Button variant="contained">Hello world</Button>
       <p>Hello</p>
       <Typography variant="body2" color="text.red">
