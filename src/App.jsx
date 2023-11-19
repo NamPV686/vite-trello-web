1d3ac8cb4c90771899315b6f685e04675859c0a0
NamPV686 <nampv686[gmail.com>
2023-11-19 13:18:43 +0700

    #9 Delete redundant content

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
-import Button from '@mui/material/Button'
 import HomeIcon from '@mui/icons-material/home'
 import { pink } from '@mui/material/colors/'
 import AccountBalance from '@mui/icons-material/accountbalance'
-import Typography from '@mui/material/Typography'
 import { useColorScheme } from '@mui/material/styles'
-import useMediaQuery from '@mui/material/useMediaQuery'
-
-import * as React from 'react'
 import InputLabel from '@mui/material/InputLabel'
 import MenuItem from '@mui/material/MenuItem'
 import FormControl from '@mui/material/FormControl'
 import Select from '@mui/material/Select'
-import { Box, ThemeProvider } from '@mui/system'
+import { Box } from '@mui/system'
 
 import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined'
 import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined'
@@ -54,39 +49,12 @@ function ModeSelect() {
   )
 }
 
-// function ModeToggle() {
-//   const { mode, setMode } = useColorScheme()
-  // const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)')
-  // const prefersLightMode = useMediaQuery('(prefers-color-scheme: light)')
-  // console.log ('prefersDarkMode: ', prefersDarkMode)
-  // console.log ('prefersLightMode: ', prefersLightMode)
-
-//   return (
-//     <Button
-//       onClick={() => {
-//         setMode(mode === 'light' ? 'dark' : 'light')
-//       }}
-//     >
-//       {mode === 'light' ? 'Turn dark' : 'Turn light'}
-//     </Button>
-//   )
-// }
-
-
 function App() {
 
   return (
     <>
-      {/* <ModeToggle /> */}
-      <hr />
       <ModeSelect/>
       <hr />
-      <Button variant="contained">Hello world</Button>
-      <p>Hello</p>
-      <Typography variant="body2" color="text.red">
-        h1. Heading
-      </Typography>
-
       <HomeIcon />
       <HomeIcon color="primary" />
       <HomeIcon color="secondary" />
