d373b24710aa4cf57285d19c5d5fd77ab31ecd14
NamPV686 <nampv686[gmail.com>
2023-11-24 17:04:12 +0700

    #26. Code Board Content (Columns & Cards)

--- src/theme.js
+++ src/theme.js
@@ -49,7 +49,7 @@ const theme = extendTheme({
     MuiTypography: {
       styleOverrides: {
         root: {
-          '&.MuiTypography-body1': { fontSize: '0.875rem'}
+          '&.MuiTypography-body1': { fontSize: '0.875rem' }
         }
       }
     },
