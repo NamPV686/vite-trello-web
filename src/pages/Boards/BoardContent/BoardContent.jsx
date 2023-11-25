51439c393bb7f4b7c7f44156b4b07ea40283a24c
NamPV686 <nampv686[gmail.com>
2023-11-25 21:07:22 +0700

    #30. Keo tha Trello Columns | Mouse - Touch - Pen

--- src/pages/Boards/BoardContent/BoardContent.jsx
+++ src/pages/Boards/BoardContent/BoardContent.jsx
@@ -1,30 +1,66 @@
 import { Box } from '@mui/system'
 import ListColums from './ListColumns/ListColumns'
 import { mapOrder } from '~/utils/sorts'
+import { DndContext, PointerSensor, TouchSensor, MouseSensor, useSensor, useSensors } from '@dnd-kit/core'
+import { arrayMove } from '@dnd-kit/sortable'
+import { useEffect, useState } from 'react'
 
 function BoardContent(props) {
   const { board } = props
-  const orderedColumns = mapOrder(board?.columns, board?.columnOrderIds, '_id')
+  //Yêu cầu di chuyển chuột 10px thì mới kick hoạt event
+  // const pointerSensor = useSensor(PointerSensor, { activationConstraint: { distance: 10 } })
+
+  //Yêu cầu di chuyển chuột 10px thì mới kick hoạt event
+  const mouseSensor = useSensor(MouseSensor, { activationConstraint: { distance: 10 } })
+
+  //Nhấn giữ 250ms và dung sai của cảm ứng khoảng 0-500 thì mới kích hoạt even
+  const touchSensor = useSensor(TouchSensor, { activationConstraint: { delay: 250, tolerance: 500 } })
+
+  //Ưu tiên sử dụng kết hợp 2 loại sensor là MouseSensor và TouchSensor để có trải nghiệm tốt trên mobile
+  // const sensors = useSensors(pointerSensor)
+  const sensors = useSensors(mouseSensor, touchSensor)
+
+  const [orderedColumns, setOrderedColumns] = useState([])
+
+  useEffect(() => {
+    setOrderedColumns(mapOrder(board?.columns, board?.columnOrderIds, '_id'))
+  }, [board])
+  const handleDragEnd = (event) => {
+    const { active, over } = event
+    console.log(event)
+    //Kiểm tra nếu không tồn tại over(kéo thả linh tinh) return luôn tránh lỗi
+    if (!over) return
+
+    //Nếu vị trí sau khi kéo thả khác vị trí ban đầu
+    if (active.id != over.id) {
+      //Lấy vị trí cũ từ thằng active
+      const oldIndex = orderedColumns.findIndex(c => c._id === active.id)
+      //Lấy vị trí mới từ thằng over
+      const newIndex = orderedColumns.findIndex(c => c._id === over.id)
+
+      //Dùng arrayMove để sắp sếp lại mảng Columns ban đầu
+      const dndOrderedColumns = arrayMove(orderedColumns, oldIndex, newIndex)
+      //Sau này sử dụng để xử lý khi gọi API
+      // const dndOrderedColumnsIds = dndOrderedColumns.map(c => c._id)
+      // console.log('dndOrderedColumns', dndOrderedColumns)
+      // console.log('dndOrderedColumnsIds', dndOrderedColumnsIds)
+
+      //Cập nhật lại state columns ban đầu sau khi đã kéo thả
+      setOrderedColumns(dndOrderedColumns)
+    }
+  }
   return (
-    <Box sx={{
-      bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#34495e' : '#1976d2'),
-      width: '100%',
-      height: (theme) => theme.trello.boardContentHeight,
-      p: '10px 0'
-    }}>
+    <DndContext onDragEnd={handleDragEnd} sensors={sensors}>
       <Box sx={{
-        bgcolor: 'inherit',
+        bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#34495e' : '#1976d2'),
         width: '100%',
-        height: '100%',
-        display: 'flex',
-        overflowX: 'auto',
-        overflowY: 'hidden',
-        '&::-webkit-scrollbar-track': { m: 2 }
+        height: (theme) => theme.trello.boardContentHeight,
+        p: '10px 0'
       }}>
         {/* ListColums */}
         <ListColums columns={orderedColumns}/>
       </Box>
-    </Box>
+    </DndContext>
   )
 }
 
--- src/pages/Boards/BoardContent/ListColumns/ListColumns.jsx
+++ src/pages/Boards/BoardContent/ListColumns/ListColumns.jsx
@@ -2,45 +2,58 @@ import { Box } from '@mui/system'
 import Colums from './Colums/Colums'
 import { Button } from '@mui/material'
 import NoteAddIcon from '@mui/icons-material/NoteAdd'
+import { SortableContext, horizontalListSortingStrategy } from '@dnd-kit/sortable'
 
 function ListColums(props) {
   const { columns } = props
   return (
-    <Box sx={{
-      bgcolor: 'inherit',
-      width: '100%',
-      height: '100%',
-      display: 'flex',
-      overflowX: 'auto',
-      overflowY: 'hidden',
-      '&::-webkit-scrollbar-track': { m: 2 }
-    }}>
-      {columns?.map((column) => {
-        return (
-          <Colums key={column._id} column={column} />
-        )
-      })}
-      {/* Add new Column */}
+    <SortableContext items={columns?.map(c => c._id)} strategy={horizontalListSortingStrategy}>
       <Box sx={{
-        minWidth: '200px',
-        maxWidth: '200px',
-        mx: 2,
-        borderRadius: '6px',
-        height: 'fit-content',
-        bgcolor: '#ffffff3d'
+        bgcolor: 'inherit',
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        overflowX: 'auto',
+        overflowY: 'hidden',
+        '&::-webkit-scrollbar-track': { m: 2 }
       }}>
-        <Button
-          startIcon={<NoteAddIcon />}
-          sx={{
-            color: 'white',
-            width: '100%',
-            justifyContent: 'flex-start',
-            pl: 2.5,
-            py: 1
-          }}
-        >Add new column</Button>
+        <Box sx={{
+          bgcolor: 'inherit',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          overflowX: 'auto',
+          overflowY: 'hidden',
+          '&::-webkit-scrollbar-track': { m: 2 }
+        }}>
+          {columns?.map((column) => {
+            return (
+              <Colums key={column._id} column={column} />
+            )
+          })}
+          {/* Add new Column */}
+          <Box sx={{
+            minWidth: '200px',
+            maxWidth: '200px',
+            mx: 2,
+            borderRadius: '6px',
+            height: 'fit-content',
+            bgcolor: '#ffffff3d'
+          }}>
+            <Button
+              startIcon={<NoteAddIcon />}
+              sx={{
+                color: 'white',
+                width: '100%',
+                justifyContent: 'flex-start',
+                pl: 2.5,
+                py: 1
+              }}
+            >Add new column</Button>
+          </Box>
+        </Box>
       </Box>
-    </Box>
+    </SortableContext>
   )
 }
 
